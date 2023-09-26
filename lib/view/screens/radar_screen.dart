e6df026ab53df1a99502e1638642a15a33a21dbd
Jackie Jensen <Jackiehartvig@hotmail.com>
2023-09-26 12:38:28 +0200

    added pictures of util, added top bar, with colors and line

--- lib/view/screens/radar_screen.dart
+++ lib/view/screens/radar_screen.dart
@@ -1,10 +1,10 @@
 import 'package:cs_smoke_app/view/shared/global.dart';
-import 'package:cs_smoke_app/view/widgets/app_bar_widget.dart';
 import 'package:cs_smoke_app/view/widgets/raw_gesture_detector_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../core/viewmodels/util_view_model.dart';
+import '../widgets/app_bar_widget.dart';
 import '../widgets/grid_view_widget.dart';
 
 class RadarScreen extends StatelessWidget {
@@ -15,52 +15,65 @@ class RadarScreen extends StatelessWidget {
     final utilViewModel = Provider.of<UtilViewModel>(context);
 
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(200),
+      /*appBar: PreferredSize(
+        preferredSize: Size.fromHeight(180),
         child: AppBarWidget(),
-      ),
-      body: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(40),
-          topRight: Radius.circular(40),
-        ),
-        child: Stack(
-          children: [
-            Container(
-              color: Global.bgColor,
-              child: const Center(
-                  child: RawGestureDetectorWidget(child: GridViewWidget())),
-            ),
-            utilViewModel.isUtilSelected
-                ? SafeArea(
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: IconButton(
-                        icon: const Icon(
-                          Icons.close,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          utilViewModel.reset();
-                        },
-                        iconSize: 24,
-                        splashRadius: 10,
-                        style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                          ),
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Global.blue),
-                        ),
-                      ),
+      ),*/
+      body: Stack(
+        children: [
+          AppBarWidget(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: MediaQuery.of(context).size.height - 200,
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      color: Global.bgColor,
+                      child: const Center(
+                          child: RawGestureDetectorWidget(
+                              child: GridViewWidget())),
                     ),
-                  )
-                : Container(),
-          ],
-        ),
+                    utilViewModel.isUtilSelected
+                        ? SafeArea(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.close,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  utilViewModel.reset();
+                                },
+                                iconSize: 24,
+                                splashRadius: 10,
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                  ),
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Global.blue),
+                                ),
+                              ),
+                            ),
+                          )
+                        : Container(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/view/widgets/app_bar_widget.dart
+++ lib/view/widgets/app_bar_widget.dart
@@ -1,4 +1,3 @@
-import 'package:cs_smoke_app/view/shared/global.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends StatelessWidget {
@@ -6,27 +5,136 @@ class AppBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      elevation: 0,
-      backgroundColor: Colors.transparent,
-      bottom: PreferredSize(
-        preferredSize: Size.zero,
-        child: Column(
-          children: [
-            Text(
-              'Radar Screen',
-              style: TextStyle(
-                  color: Global.blue,
-                  fontWeight: FontWeight.w900,
-                  fontSize: 16.0),
+    return Container(
+      height: 240,
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              height: 260,
+              width: (MediaQuery.of(context).size.width / 2) + 28,
+              child: ClipPath(
+                clipper: SkewedClipper(true),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Colors.transparent,
+                        Colors.red.withOpacity(0.5),
+                      ],
+                    ),
+                  ),
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 18.0, bottom: 50),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            UtilButton(
+                              imagePath: 'assets/icons/smoke util.png',
+                              isSelected: false,
+                            ),
+                            UtilButton(
+                              imagePath: 'assets/icons/flash util.png',
+                              isSelected: false,
+                            ),
+                            UtilButton(
+                              imagePath: 'assets/icons/molotov util t.png',
+                              isSelected: false,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            Icon(
-              Icons.arrow_drop_down,
-              color: Global.blue,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              height: 260,
+              width: (MediaQuery.of(context).size.width / 2) + 28,
+              child: ClipPath(
+                clipper: SkewedClipper(false),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Colors.blue.withOpacity(0.5),
+                        Colors.transparent,
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class UtilButton extends StatelessWidget {
+  const UtilButton({
+    super.key,
+    required this.imagePath,
+    required this.isSelected,
+  });
+
+  final imagePath;
+  final isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+        color: Colors.grey.withOpacity(0.4),
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.grey, width: 2),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Image.asset(imagePath),
+      ),
+    );
+  }
+}
+
+class SkewedClipper extends CustomClipper<Path> {
+  final bool skewLeft;
+
+  SkewedClipper(this.skewLeft);
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    if (skewLeft) {
+      path.lineTo(0, size.height);
+      path.lineTo(size.width, size.height);
+      path.lineTo(size.width - 60, 0);
+    } else {
+      path.lineTo(60, size.height);
+      path.lineTo(size.width, size.height);
+      path.lineTo(size.width, 0);
+    }
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
