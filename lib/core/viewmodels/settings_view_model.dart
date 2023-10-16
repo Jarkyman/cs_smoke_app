f7bb02b9c667fc36c09489cda3153acaf52d9f33
Jackie Jensen <Jackiehartvig@hotmail.com>
2023-10-16 19:54:21 +0200

    saved notification status in memery

--- lib/core/viewmodels/settings_view_model.dart
+++ lib/core/viewmodels/settings_view_model.dart
@@ -1,25 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/notification_api.dart';
 
 class SettingsViewModel extends ChangeNotifier {
   SettingsViewModel() {
+    loadSettings();
     checkPermission();
   }
 
   bool _isNotification = true;
   bool get isNotification => _isNotification;
+  String _notificationKey = 'notification';
+
+  Future<void> loadSettings() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    _isNotification = prefs.getBool(_notificationKey) ?? true;
+    print('Load: $_isNotification');
+  }
+
+  void saveSettings() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    print('Save: $_isNotification');
+    await prefs.setBool(_notificationKey, _isNotification);
+  }
 
   void toggleNotification() async {
     var status = await Permission.notification.status;
     if (_isNotification) {
       print('off');
       _isNotification = false;
+      saveSettings();
     } else {
       if (status.isGranted) {
         print('on');
         _isNotification = true;
+        saveSettings();
       } else {
         print('Not granted');
         askPermission();
@@ -36,12 +53,14 @@ class SettingsViewModel extends ChangeNotifier {
     } else {
       print('premission OFF');
       _isNotification = false;
+      saveSettings();
       notifyListeners();
     }
 
     if (await Permission.notification.isRestricted) {
       print('premission Restrcted');
       _isNotification = false;
+      saveSettings();
       notifyListeners();
     }
   }
@@ -55,17 +74,19 @@ class SettingsViewModel extends ChangeNotifier {
     } else {
       print('No premission');
       _isNotification = false;
+      saveSettings();
       notifyListeners();
     }
   }
 
-  void callNotification() {
+  void callNotification() async {
+    await loadSettings();
     if (_isNotification) {
       NotificationApi.showScheduledNotification(
         title: 'Go practice now',
         body: 'Check out this new smokes on Inferno',
         payload: 'Inferno',
-        scheduledDate: DateTime.now().add(Duration(seconds: 10)),
+        scheduledDate: DateTime.now().add(Duration(seconds: 4)),
       );
     }
   }
