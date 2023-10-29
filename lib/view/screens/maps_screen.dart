76084e4e918b9e03d65084f44a104aae8ec67b30
Jackie Jensen <Jackiehartvig@hotmail.com>
2023-10-29 12:37:03 +0100

    updted event log and testet it works

--- lib/view/screens/maps_screen.dart
+++ lib/view/screens/maps_screen.dart
@@ -99,12 +99,9 @@ class _MapsScreenState extends State<MapsScreen> {
                   ),
                 ),
               );
-              await FirebaseAnalytics.instance.logSelectItem(
-                itemListId: Global.maps[index],
-              );
-              /*await FirebaseAnalytics.instance.logEvent(
+              await FirebaseAnalytics.instance.logEvent(
                 name: Global.maps[index],
-              );*/
+              );
             },
             child: Container(
               height: Dimensions.height20 * 8, // Juster højden efter behov
